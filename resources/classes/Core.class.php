3c452dc30718da66203f1aa715d6daac09e72844
Bernd Alter <bernd.alter@votum.de>
2016-04-20 19:01:32 +0200

    Revert "fixed demo mode check"
    This reverts commit 4a42fdc8bc6a03ebe8de5e495cea0582ac562f7a.

--- resources/classes/Core.class.php
+++ resources/classes/Core.class.php
@@ -229,7 +229,7 @@ class Core {
      * @access public
      */
 	public static function checkDemoMode() {
-		return (bool) self::$demoMode;
+		return (self::$demoMode) ? "true" : "false";
 	}
 
 	/**
