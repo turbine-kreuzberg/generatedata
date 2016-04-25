1e3f635596000de7a640d8255e5a2e1e4a7fe582
Bernd Alter <bernd.alter@votum.de>
2016-04-25 18:32:28 +0200

    [FEATURE] new data type 'Product'

---
+++ plugins/dataTypes/Product/lang/de.php
@@ -0,0 +1,16 @@
+<?php
+
+$L = array();
+
+$L["DATA_TYPE"] = array(
+    "NAME" => "Produkt",
+    "DESC" => "Gibt Daten von zufällig ausgewählten Produkten zurück."
+);
+
+$L["ProductId"] = "Produkt-Id";
+$L["ProductName"] = "Name";
+$L["ProductImage"] = "Bild";
+
+$L["help_intro"] = "Dieser Datentyp gibt die Daten eines zufällig ausgewählten Produkts zurück.";
+$L["incomplete_fields"] = "Bitte wählen Sie ein Produkt-Feld für die folgenden Zeilen aus:";
+
